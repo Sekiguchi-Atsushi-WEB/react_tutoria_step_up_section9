bcdc4dd45208a5d562e90579551857a94978b544
Sekiguchi-Atsushi-WEB <aaa.sekiguchi@gmail.com>
2025-12-25 13:59:17 +0900

    セクション10

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import "./App.css";
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
 
 function App() {
   return (
     <>
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
+      <h1>Hello, World!</h1>
     </>
   );
 }
