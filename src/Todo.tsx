e417e85f95a162dd4bf20b1410d4f8e3c06834a7
Sekiguchi-Atsushi-WEB <aaa.sekiguchi@gmail.com>
2025-12-25 17:44:52 +0900

    55. 型定義を効率的に管理しよう（ Pickした後）

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,8 @@
 import type { TodoType } from "./types/todo";
 
-export const Todo = (props: TodoType) => {
+export const Todo = (
+  props: Pick<TodoType, "title" | "userId" | "completed">
+) => {
   const { title, userId, completed = false } = props;
   const completeMark = completed ? "[完了]" : "[未完了]";
   return <p>{`${completeMark} ${title}（ユーザー：${userId}）`}</p>;
